7dcb4b0cdb6fb06ec25a91ccf3aaecaa265c7127
wr42pw6p4n-debug <wr42pw6p4n@privaterelay.appleid.com>
2026-09-05 12:01:21 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "rkey-job-manager-v3";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+const CACHE = "rkey-job-manager-v4";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./logo-header.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
